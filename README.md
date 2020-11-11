eb38a5e2dfa988c834598791e11900904b76870e
Sebastian Beyvers <sb@pus.de>
2020-11-11 13:54:55 +0100

    Add README.md and LICENSE.txt.

---
+++ README.md
@@ -0,0 +1,3 @@
+### KubeIT - A simple iterator framework to parallelize biological analysis in kubernetes
+
+
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"argo-clientgo-test/kubectl"
 	"fmt"
 	"io/ioutil"
+	"kubeIT/kubectl"
 	"log"
 	"time"
 )
